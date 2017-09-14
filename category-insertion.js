4955885b2ab276851567d59e52fd4fc6a011763e
rdhk <rdhk94@gmail.com>
2017-09-14 15:42:03 +0530

    Minor bug fix in script.

--- category-insertion.js
+++ category-insertion.js
@@ -2,6 +2,7 @@ const pratilipisToAdd = require('./json-data/added_categories');
 const pratilipisToUpdate = require('./json-data/updated_categories');
 const pratilipisToDelete = require('./json-data/deleted_categories');
 var parameterStoreAccessor = require('./helpers/ParameterStoreAccessor');
+var CategoryService;
 function startInsertion() {
   CategoryService.addNewSystemCategories(pratilipisToAdd)
     .then(() => {
@@ -26,7 +27,7 @@ parameterStoreAccessor.getMySqlDbCredentials()
   .then(config => {
     Object.assign(process.env, config);
     var models = require('./sequelize-models');
-    var CategoryService = require('./CategoryIngestionService');
+    CategoryService = require('./CategoryIngestionService');
     return models.sequelize.authenticate();
   })
   .then(() => {
